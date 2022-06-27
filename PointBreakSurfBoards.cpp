1e7d7d9f6480e42901b67004ce68113a920f2696
Issa <98367269+IssaPortfolio@users.noreply.github.com>
2022-06-26 21:58:19 -0400

    Update PointBreakSurfBoards.cpp

--- PointBreakSurfBoards.cpp
+++ PointBreakSurfBoards.cpp
@@ -1,15 +1,29 @@
 /*///////////////////////////////////////////////////////////////////////////////////////////////////////////
 /                                                                                                           /
 /                                                                                                           /
-/    Johnny Utah owns a surf shop on the beach called ìPoint Break Surf Shopî.                              /
-/    Johnny sells surfboards in 3 three sizes: small (2 meters), medium (3 meters), and large (4 meters).   / 
+/    Johnny Utah owns a surf shop on the beach called ‚ÄúPoint Break Surf Shop‚Äù.                              /
+/    Johnny sells surfboards in 3 three sizes: small (2 meters), medium (3 meters), and large (4 meters).   /
 /    The cost of one small surfboard is $175.00, one medium is $190.00, and one large is $200.00.           /
-/    Write a program that will make the surf shop operational.                                              /  
+/    Write a program that will make the surf shop operational.                                              /
 /    Your program should allow the user to do the following:                                                /
 /                                                                                                           /
 /        1. Buy any surfboard in any size and in any quanity.                                               /
 /        2. At any time show the total number of surfboards of each size sold.                              /
 /        3. At any time show the total money made.                                                          /
+/                                                                                                           /
+/   Your program must consist of "int main()" and at least the following functions:                         /
+/                                                                                                           /
+/   void ShowUsage();                                                                                       /
+/       a function to show the user how to use the program.                                                 /
+/                                                                                                           /
+/   void MakePurchase(int& iTotalSmall, int& iTotalMedium, int& iTotalLarge);                               /
+/       a function to sell surfboards.                                                                      /
+/                                                                                                           /
+/   void DisplayPurchase(const int iTotalSmall, const int iTotalMedium, const int iTotalLarge);             /
+/       function to show the number of surfboards of each size sold.                                        /
+/                                                                                                           /
+/   void DisplayTotal(const int iTotalSmall, const int iTotalMedium, const int iTotalLarge);                /
+/       a function to show the total amount of money made./                                                 /
 /                                                                                                           /
 /                                                                                                           /
 //////////////////////////////////////////////////////////////////////////////////////////// BY ISSA HABEEB*/
